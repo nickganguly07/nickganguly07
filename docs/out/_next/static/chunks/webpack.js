b8b6ea340e3ec774e22e2fa52a669ef04672fe8b
Nick Ganguly <nick@cyboticx.com>
2020-08-06 23:19:04 +0530

    Deploy Next.js to gh-pages

--- docs/out/_next/static/chunks/webpack.js
+++ docs/out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8990d60a303c5ea2c285";
+/******/ 	var hotCurrentHash = "bcaa5eeaadc887de6fa2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
